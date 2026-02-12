36188cb8c82d22fd503bd375be98a5fc9db20d7f
Zaid Shaikh <zaid.shaikh@kanini.com>
2026-02-12 18:17:38 +0530

    port updated

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import uvicorn
 from synaptic.config import settings
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "synaptic.api.app:app",
         host="0.0.0.0",
-        port=port,
+        port=8000,
         reload=True,
         log_level=settings.log_level.lower(),
     )
